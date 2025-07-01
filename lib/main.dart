2df4257fd6871a0d5bd39b4dc3b1d1dc36fee850
NoorDiyana1100 <noordiyana02@gmail.com>
2025-07-02 01:38:46 +0800

    part diyana
    community + recipe details part 2

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class _BottomNavBarExampleState extends State<BottomNavBarExample> {
 
   final List<Widget> _pages = [
     const HomepageScreen(),
-    const CommunityScreen(),
+    CommunityScreen(),
     RecipeCreationScreen(), // Use the correct class name as defined in createrecipe.dart
     const ViewProfileScreen(),
   ];
